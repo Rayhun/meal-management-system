fb75d4bbf75a9095478639fb8f8957e63371c58b
Rayhun <rayhunkhan27@gmail.com>
2022-03-11 17:10:40 +0600

    add dashboard todo

--- meal/views/dashboard.py
+++ meal/views/dashboard.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
+from meal.models.market import ToDo
+
 
 class DashboardView(
     LoginRequiredMixin, UserPassesTestMixin, TemplateView
@@ -9,7 +11,9 @@ class DashboardView(
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Dashboard'
+        context['todo_list'] = ToDo.objects.filter(
+            user=self.request.user
+        ).order_by('-created_at')
         return context
 
     def test_func(self):
