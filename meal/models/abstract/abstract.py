5eb73f34c9fd2228550f4aca505f25aa8abbdf60
Rayhun <rayhunkhan27@gmail.com>
2022-03-11 15:37:02 +0600

    add models and update models

--- meal/models/abstract/abstract.py
+++ meal/models/abstract/abstract.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class AbstractBaseModel(models.Model):
@@ -6,8 +7,20 @@ class AbstractBaseModel(models.Model):
     Abstract base model for all models.
     """
     created_at = models.DateTimeField(auto_now_add=True)
+    created_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
+        blank=True, null=True, related_name="%(app_label)s_%(class)s_createdby"
+    )
     updated_at = models.DateTimeField(auto_now=True)
+    updated_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
+        blank=True, null=True, related_name="%(app_label)s_%(class)s_updated"
+    )
     delete_at = models.DateTimeField(blank=True, null=True)
+    deleted_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
+        blank=True, null=True, related_name="%(app_label)s_%(class)s_deleted"
+    )
     is_active = models.BooleanField(default=True)
     is_delete = models.BooleanField(default=False)
 
--- meal/models/market.py
+++ meal/models/market.py
@@ -29,3 +29,46 @@ class ToDo(AbstractBaseModel):
 
     def __str__(self):
         return self.name
+
+
+class Item(AbstractBaseModel):
+    """
+    Item model.
+    """
+    name = models.CharField(max_length=255)
+    quantity = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class NeedItem(models.Model):
+    """
+    NeedItem model.
+    """
+    name = models.CharField(max_length=255)
+    quantity = models.CharField(max_length=255)
+    date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.item.name
+
+
+class Market(AbstractBaseModel):
+    """
+    Market model.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    item = models.ForeignKey(
+        Item, on_delete=models.SET_NULL, null=True
+    )
+    total_cost = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0.00
+    )
+    date = models.DateField(blank=True)
+
+    def __str__(self):
+        return self.name
